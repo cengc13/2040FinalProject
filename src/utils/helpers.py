a1f0e6aa9ed377a7383f12b3a52b995ef695867b
Cheng Zeng <cheng_zeng1@brown.edu>
2020-04-23 18:05:08 -0400

    updated utils

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -2,6 +2,7 @@
 import numpy as np
 from tensorflow.keras.callbacks import Callback
 from sklearn.metrics import accuracy_score, roc_auc_score
+import re
 
 def regular_encode(texts, tokenizer, maxlen=512):
     """
@@ -72,3 +73,159 @@ class RocAucEvaluation(Callback):
             y_pred = self.model.predict(self.X_val, verbose=0)
             score = roc_auc_score(self.y_val, y_pred)
             print("\n ROC-AUC - epoch: {:d} - score: {:.6f}".format(epoch+1, score))
+            
+
+def build_lrfn(strategy, lr_start=0.000001, lr_max=0.000002, 
+               lr_min=0.0000001, lr_rampup_epochs=7, 
+               lr_sustain_epochs=0, lr_exp_decay=.87):
+    lr_max = lr_max * strategy.num_replicas_in_sync
+
+    def lrfn(epoch):
+        if epoch < lr_rampup_epochs:
+            lr = (lr_max - lr_start) / lr_rampup_epochs * epoch + lr_start
+        elif epoch < lr_rampup_epochs + lr_sustain_epochs:
+            lr = lr_max
+        else:
+            lr = (lr_max - lr_min) * lr_exp_decay**(epoch - lr_rampup_epochs - lr_sustain_epochs) + lr_min
+        return lr
+    
+    return lrfn
+
+    
+##### Text cleaning
+def clean(text):
+    text = text.fillna("fillna").str.lower()
+    text = text.map(lambda x: re.sub('\\n',' ',str(x)))
+    text = text.map(lambda x: re.sub("\[\[User.*",'',str(x)))
+    text = text.map(lambda x: re.sub("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",'',str(x)))
+    text = text.map(lambda x: re.sub("\(http://.*?\s\(http://.*\)",'',str(x)))
+    return text
+
+# https://www.kaggle.com/chenshengabc/from-quest-encoding-ensemble-a-little-bit-differen
+
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', '\xa0', '\t',
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', '\u3000', '\u202f',
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', '«',
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+ 
+mispell_dict = {"aren't" : "are not",
+"can't" : "cannot",
+"couldn't" : "could not",
+"couldnt" : "could not",
+"didn't" : "did not",
+"doesn't" : "does not",
+"doesnt" : "does not",
+"don't" : "do not",
+"hadn't" : "had not",
+"hasn't" : "has not",
+"haven't" : "have not",
+"havent" : "have not",
+"he'd" : "he would",
+"he'll" : "he will",
+"he's" : "he is",
+"i'd" : "I would",
+"i'd" : "I had",
+"i'll" : "I will",
+"i'm" : "I am",
+"isn't" : "is not",
+"it's" : "it is",
+"it'll":"it will",
+"i've" : "I have",
+"let's" : "let us",
+"mightn't" : "might not",
+"mustn't" : "must not",
+"shan't" : "shall not",
+"she'd" : "she would",
+"she'll" : "she will",
+"she's" : "she is",
+"shouldn't" : "should not",
+"shouldnt" : "should not",
+"that's" : "that is",
+"thats" : "that is",
+"there's" : "there is",
+"theres" : "there is",
+"they'd" : "they would",
+"they'll" : "they will",
+"they're" : "they are",
+"theyre":  "they are",
+"they've" : "they have",
+"we'd" : "we would",
+"we're" : "we are",
+"weren't" : "were not",
+"we've" : "we have",
+"what'll" : "what will",
+"what're" : "what are",
+"what's" : "what is",
+"what've" : "what have",
+"where's" : "where is",
+"who'd" : "who would",
+"who'll" : "who will",
+"who're" : "who are",
+"who's" : "who is",
+"who've" : "who have",
+"won't" : "will not",
+"wouldn't" : "would not",
+"you'd" : "you would",
+"you'll" : "you will",
+"you're" : "you are",
+"you've" : "you have",
+"'re": " are",
+"wasn't": "was not",
+"we'll":" will",
+"didn't": "did not",
+"tryin'":"trying"}
+
+def clean_text(x):
+    x = str(x).replace("\n","")
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
+
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+
+### clean text from the kernel:
+### https://www.kaggle.com/mobassir/understanding-cross-lingual-models#Part-2-:-Implementation-using-TPU-Multiprocessing
+from nltk.tokenize.treebank import TreebankWordTokenizer
+
+
+def handle_contractions(x):
+    tokenizer = TreebankWordTokenizer()
+    x = tokenizer.tokenize(x)
+    return x
+
+def fix_quote(x):
+    x = [x_[1:] if x_.startswith("'") else x_ for x_ in x]
+    x = ' '.join(x)
+    return x
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+
+def replace_typical_misspell(text):
+    mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+    def replace(match):
+        return mispellings[match.group(0)]
+
+    return mispellings_re.sub(replace, text)
+
+
+def clean_data(df, columns: list):
+    for col in columns:
+#         df[col] = df[col].apply(lambda x: clean_numbers(x))
+        df[col] = df[col].apply(lambda x: clean_text(x.lower())) 
+        df[col] = df[col].apply(lambda x: replace_typical_misspell(x))
+        df[col] = df[col].apply(lambda x: handle_contractions(x))  
+        df[col] = df[col].apply(lambda x: fix_quote(x))   
+    
+    return df
